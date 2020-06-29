4d055ff79a5e31fd2f4f7de215030984eb301414
karlmiche <karlymism@gmail.com>
2020-06-29 16:57:56 -0700

    filling out authentication routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require("../models");
 //import middleware
 const flash = require("flash");
+const passport;
 
 //register get route
 router.get("/register", function(req, res) {
@@ -44,8 +45,31 @@ router.get("/login", function(req, res) {
 
 //post route
 router.post("/login", function(req, res){
-    res.render("auth/login")
+    passport.authenticate("local", function(error, user, info){
+        //if no user authenticated
+        if (!user) {
+            req.flash("error", "Invalid username or password");
+            //we are going to save our usersession no username
+            //redirect our user to try logging in again
+        }
+        if (error) {
+            //TODO: add next param from function
+            return error;
+        }
+        req.login(function(user, error){
+            //if error, move to error
+            //if success, flash success message
+            //if success save session and redirect user
+        })
+    })
 })
+
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/login",
+    successFlash: "Welcome to our app!",
+    failureFlash: "Invalid username or password."
+}));
 
 //export router
 module.exports = router;
