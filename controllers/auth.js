f9dce87cde51830705da3bff1be656401a8772e1
karlmiche <karlymism@gmail.com>
2020-06-29 15:52:36 -0700

    POST route for registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const expresss = require("express");
+const express = require("express");
 const router = express.Router();
 const db = require("../models");
 //import middleware
+const flash = require("flash");
 
 //register get route
 router.get("/register", function(req, res) {
@@ -10,12 +11,41 @@ router.get("/register", function(req, res) {
 
 //register post route
 router.post("/register", function(req, res) {
-    res.render()
+    db.user.findOrCreate({
+        where: {
+            email: req.body.email
+        }, defaults: {
+            name: req.body.name,
+            password: req.body.password
+        }
+    }).then(function(user, created) {
+        //if user was created
+        if (created){
+            
+            //authenticate user and start authorization process
+            console.log("User created 🥥");
+            res.redirect("/"); 
+        } else {
+            console.log("User email already exists. 🏴‍☠️");
+            req.flash("error", "Error: email already exists for user. Try again.");
+            res.redirect("/auth/register");
+        }
+    }).catch(function(error){
+        console.log(`Error found. \nMessage: ${error.message}. Please review - ${error}`);
+        req.flash("error", error.message);
+        res.redirect("auth/register");
+    })
 })
 
 //login get route
 router.get("/login", function(req, res) {
     res.render("auth/login")
 })
-//post route
 
+//post route
+router.post("/login", function(req, res){
+    res.render("auth/login")
+})
+
+//export router
+module.exports = router;
